31c486b83f2e509bee045bb80992d80e979a6928
Tereza Opanska <TNOpanska19@codingburgas.bg>
2021-04-22 22:01:07 +0300

    Sort JS files

--- JS/form_script.js
+++ JS/form_script.js
@@ -1,3 +1,10 @@
+const toggleButton = document.getElementsByClassName('button')[0]
+const navbarLinks = document.getElementsByClassName('links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
 function alertSubmissionMessage () {
     alert("Благодарим Ви, че се свързахте с нас!");
 }
